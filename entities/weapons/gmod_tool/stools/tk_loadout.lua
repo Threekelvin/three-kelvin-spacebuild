26fc1bf58e97a252f137fd976f3e07154c7958a4
randomic <j.a.d.mcmillan@gmail.com>
2013-02-18 22:58:27 +0000

    Added rebuild time for respawning loadout entities after they have been removed.

--- entities/weapons/gmod_tool/stools/tk_loadout.lua
+++ entities/weapons/gmod_tool/stools/tk_loadout.lua
@@ -116,15 +116,64 @@ if CLIENT then
             self:Clear(true)
             self.SelectedPanel = nil
             RunConsoleCommand("tk_loadout_model", "")
+			
             local loadout = TK.DB:GetPlayerData("player_loadout")
+			local uid = LocalPlayer():GetNWString("UID")
+			local spawned = TK.LO.SpawnedEnts[uid] || {}
+			local building = TK.LO.BuildingEnts[uid] || {}
+			local icons = {}
+			for slot,id in pairs(loadout) do
+				local found = false
+				if string.match(slot, "[%w]+$") == "item" && id != 0 then
+					for k,v in pairs(building) do
+						if v.id == id then
+							found = true
+							table.insert(icons, {['id']=id, ['time']=v.time})
+							break
+						end
+					end
+					if found then continue end
+					for k,v in pairs(spawned) do
+						if v.itemid == id then
+							found = true
+							table.insert(icons, {['id']=id, ['time']=-1})
+							break
+						end
+					end
+					if !found then table.insert(icons, {['id']=id, ['time']=0}) end
+				end
+			end
             
-            for k,v in pairs(loadout) do
-                if string.match(k, "[%w]+$") != "item" then continue end
-                if v == 0 then continue end
+            for k,v in pairs(icons) do
                 
-                local item = TK.TD:GetItem(v)
+                local item = TK.TD:GetItem(v.id)
                 local icon = vgui.Create("SpawnIcon")
-				icon.DoRightClick = function()
+				local overlay = vgui.Create("DButton", icon)
+				overlay:SetText("")
+				local w, h = icon:GetSize()
+				overlay:SetSize( w, h )
+				local r = w+h
+				local cos, sin = math.cos, math.sin
+				function overlay:Paint()
+					local A = 0.0
+					if v.time == 0 then A = 1.0
+					elseif v.time > 0 then A = (v.time-CurTime())/TK.LO.RebuildTime end
+					A = A * 2 * math.pi
+					local verticies = {
+						{x = w/2, y = h/2},
+						{x = w/2, y = 0},
+					}
+					for i=1,4 do
+						table.insert( verticies, {x = r*cos(i*A/4) + w/2, y = r*sin(i*A/4) + h/2} )
+					end
+					surface.SetDrawColor( 0,0,0,100 )
+					surface.DrawPoly( verticies )
+					surface.DrawRect( 0, 0, w, h )
+				end
+				function overlay:DoClick()
+					self:GetParent():DoClick()
+				end
+				function overlay:DoRightClick()
 					RunConsoleCommand( "playgamesound", "ui/buttonclickrelease.wav" )
 					SetClipboardText(item.mdl)
 					GAMEMODE:AddNotify("Model path copied to clipboard.", NOTIFY_HINT, 5)
@@ -132,11 +181,12 @@ if CLIENT then
                 icon:SetModel(item.mdl)
                 icon:SetSize(64, 64)
                 icon:SetToolTip(item.name)
-                self:AddPanel(icon, {tk_loadout_item = v, tk_loadout_model = item.mdl, playgamesound = "ui/buttonclickrelease.wav"})
+                self:AddPanel(icon, {tk_loadout_item = v.id, tk_loadout_model = item.mdl, playgamesound = "ui/buttonclickrelease.wav"})
             end
         end
         List:Populate()
         CPanel:AddItem(List)
+		TK.LO.SpawnList = List
         
         hook.Add("TKDBPlayerData", "tk_loadout", function(dtable, idx, data)
             if dtable != "player_loadout" then return end
