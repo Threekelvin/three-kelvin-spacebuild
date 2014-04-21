9c1cb2743d3453a27fa014ba504274e4a12d3422
randomic <j.a.d.mcmillan@gmail.com>
2014-04-21 21:35:23 +0100

    Made some tweaks to changelog

--- web/changelog.php
+++ web/changelog.php
@@ -25,7 +25,7 @@ function getAvatarUri($name) {
 	return $uri;
 }
 
-$top = "<html><head><style type='text/css'> html, body { background:#979797; } .entry { background:#c9c9c9; border-radius: 5px; margin:5px; padding:5px; height:auto; min-height:64px; font-family: Tahoma,Arial,sans-serif; } .avatar { float:right; } img { margin:0; } ul { margin:0; } </style><title>Changelog</title><meta http-equiv='Content-Type' content='text/html;charset=utf-8' /></head><body>";
+$top = "<html><head><style type='text/css'> html, body { background:#979797; } .entry { background:#c9c9c9; border-radius:5px; margin:5px; padding:5px; overflow:hidden; min-height:64px; font-family:Tahoma,Arial,sans-serif; } .avatar { float:right; } img { margin:0; } ul { margin:0; } </style><title>Changelog</title><meta http-equiv='Content-Type' content='text/html;charset=utf-8' /></head><body>";
 $middle = "";
 $bottom = "</body></html>";
 
@@ -49,7 +49,7 @@ for($i = 0; $i < count($lines)-1; $i++) {
 foreach($commits as $date => $users) {
 	$middle = $middle . "<div class='entry'><b>" . $date . "</b><br>";
 	foreach($users as $user => $messages) {
-		$middle = $middle . "<u>" . $user . "</u><div class='avatar'><img src='" . getAvatarUri($user) . "' width='64' height='64'/></div><br><br><ul>";
+		$middle = $middle . "<u>" . $user . "</u><div class='avatar'><img src='" . getAvatarUri($user) . "' width='64' height='64'/></div><br><ul>";
 		foreach($messages as $message) {
 			$middle = $middle . "<li>" . $message . "</li>";
 		}
