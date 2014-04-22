d4444926d80c0a11028abd9c0ab180e9e1367d1c
randomic <j.a.d.mcmillan@gmail.com>
2014-04-22 19:05:54 +0100

    Fixed a bug where avatars weren't lining up

--- web/changelog.php
+++ web/changelog.php
@@ -25,7 +25,10 @@ function getAvatarUri($name) {
 	return $uri;
 }
 
-$top = "<html><head><style type='text/css'> html, body { background:#979797; } .entry { background:#c9c9c9; border-radius:5px; margin:5px; padding:10px; overflow:hidden; min-height:64px; font-family:Tahoma,Arial,sans-serif; } .avatar { float:right; } img { margin:0; } ul { margin:0; } </style><title>Changelog</title><meta http-equiv='Content-Type' content='text/html;charset=utf-8' /></head><body>";
+$top =	  "<html><head><style type='text/css'> html, body { background:#979797; } "
+		. ".entry { background:#c9c9c9; border-radius:5px; margin:5px; padding:10px; overflow:hidden; min-height:64px; font-family:Tahoma,Arial,sans-serif; } "
+		. ".avatar { float:right; } img { margin:0; } ul { margin:0; } "
+		. "</style><title>Changelog</title><meta http-equiv='Content-Type' content='text/html;charset=utf-8' /></head><body>";
 $middle = "";
 $bottom = "</body></html>";
 
@@ -47,15 +50,15 @@ for($i = 0; $i < count($lines)-1; $i++) {
 }
 
 foreach($commits as $date => $users) {
-	$middle = $middle . "<div class='entry'><b>" . $date . "</b><br>";
+	$middle .= "<div class='entry'><b>" . $date . "</b><br>";
 	foreach($users as $user => $messages) {
-		$middle = $middle . "<u>" . $user . "</u><div class='avatar'><img src='" . getAvatarUri($user) . "' width='64' height='64'/></div><br><ul>";
+		$middle .= "<div><u>" . $user . "</u><div class='avatar'><img src='" . getAvatarUri($user) . "' width='64' height='64'/></div><br><ul>";
 		foreach($messages as $message) {
-			$middle = $middle . "<li>" . $message . "</li>";
+			$middle .= "<li>" . $message . "</li>";
 		}
-		$middle = $middle . "</ul>";
+		$middle .= "</ul></div>";
 	}
-	$middle = $middle . "</div>";
+	$middle .= "</div>";
 }
 
 $data = $top . $middle . $bottom;
