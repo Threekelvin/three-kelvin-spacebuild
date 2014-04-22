08d2e26e8cd7d868aece1971bbe0b22a4a536b2f
randomic <j.a.d.mcmillan@gmail.com>
2014-04-22 19:11:40 +0100

    Minor fix to changelog avatar display

--- web/changelog.php
+++ web/changelog.php
@@ -26,8 +26,8 @@ function getAvatarUri($name) {
 }
 
 $top =	  "<html><head><style type='text/css'> html, body { background:#979797; } "
-		. ".entry { background:#c9c9c9; border-radius:5px; margin:5px; padding:10px; overflow:hidden; min-height:64px; font-family:Tahoma,Arial,sans-serif; } "
-		. ".avatar { float:right; } img { margin:0; } ul { margin:0; } "
+		. ".entry { background:#c9c9c9; border-radius:5px; margin:5px; padding:5px; overflow:hidden; min-height:64px; font-family:Tahoma,Arial,sans-serif; } "
+		. ".avatar { float:right; } img { margin:5px; } ul { margin:0; } "
 		. "</style><title>Changelog</title><meta http-equiv='Content-Type' content='text/html;charset=utf-8' /></head><body>";
 $middle = "";
 $bottom = "</body></html>";
@@ -52,7 +52,7 @@ for($i = 0; $i < count($lines)-1; $i++) {
 foreach($commits as $date => $users) {
 	$middle .= "<div class='entry'><b>" . $date . "</b><br>";
 	foreach($users as $user => $messages) {
-		$middle .= "<div><u>" . $user . "</u><div class='avatar'><img src='" . getAvatarUri($user) . "' width='64' height='64'/></div><br><ul>";
+		$middle .= "<div style='overflow:hidden'><u>" . $user . "</u><div class='avatar'><img src='" . getAvatarUri($user) . "' width='64' height='64'/></div><br><ul>";
 		foreach($messages as $message) {
 			$middle .= "<li>" . $message . "</li>";
 		}
